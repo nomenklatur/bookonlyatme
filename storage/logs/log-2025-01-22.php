3d72ab66c71b4f5d8ed29b74d9f47cf405b9868a
Dimas <dimasekaputra2000@gmail.com>
2025-01-26 23:38:30 +0700

    initial commit

---
+++ storage/logs/log-2025-01-22.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2025-01-22 07:09:42 --> Severity: error --> Exception: No such file or directory /Users/dimas/Documents/Dev/Personal/bookonlyatme/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2025-01-22 07:12:59 --> Severity: error --> Exception: No such file or directory /Users/dimas/Documents/Dev/Personal/bookonlyatme/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2025-01-22 07:13:56 --> Severity: error --> Exception: No such file or directory /Users/dimas/Documents/Dev/Personal/bookonlyatme/system/database/drivers/mysqli/mysqli_driver.php 203
+ERROR - 2025-01-22 07:14:04 --> 404 Page Not Found: Index/index
+ERROR - 2025-01-22 07:14:08 --> Severity: error --> Exception: No such file or directory /Users/dimas/Documents/Dev/Personal/bookonlyatme/system/database/drivers/mysqli/mysqli_driver.php 203
