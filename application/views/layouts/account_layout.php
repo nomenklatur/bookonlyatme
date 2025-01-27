0f6f4ad940f98f29516c84a673af5d72493f0647
Dimas <dimasekaputra2000@gmail.com>
2025-01-27 22:22:50 +0700

    feat: setup account layout to use own brand

--- application/views/layouts/account_layout.php
+++ application/views/layouts/account_layout.php
@@ -9,10 +9,12 @@
 
     <?php slot('meta'); ?>
 
-    <title><?= vars('page_title') ?? lang('account') ?> | Easy!Appointments</title>
+    <title><?= vars('page_title') ?? lang('account') ?></title>
 
-    <link rel="icon" type="image/x-icon" href="<?= asset_url('assets/img/favicon.ico') ?>">
-    <link rel="icon" sizes="192x192" href="<?= asset_url('assets/img/logo.png') ?>">
+    <!-- <link rel="icon" type="image/x-icon" href="<?= asset_url('assets/img/favicon.ico') ?>">
+    <link rel="icon" sizes="192x192" href="<?= asset_url('assets/img/logo.png') ?>"> -->
+    <link rel="icon" sizes="192x192" href="<?= asset_url('assets/img/bookonlyatme-favicon.png') ?>">
+
 
     <link rel="stylesheet" type="text/css"
           href="<?= asset_url('assets/css/themes/' . setting('theme', 'default') . '.css') ?>">
